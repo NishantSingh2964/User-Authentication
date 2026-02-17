6f3fc9ce8ede6fd493d37ea604faa2c680042576
Nishant Raj <nishantraj7859@gmail.com>
2026-02-18 00:39:55 +0530

    Update CORS middleware configuration

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,13 @@ const app = express();
 const PORT = 4000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://user-authentication-yrl6.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 connectDB();
 
