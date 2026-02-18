76339907dfdf96ea1f52ec9da2e1c45149e5f3dd
Nishant Raj <nishantraj7859@gmail.com>
2026-02-18 11:51:33 +0530

    Update backend URL for user authentication

--- Frontend/src/Context/AuthContext.jsx
+++ Frontend/src/Context/AuthContext.jsx
@@ -5,7 +5,7 @@ export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const backendUrl = " https://user-authentication-ecru.vercel.app";
+  const backendUrl = " https://user-authentication-ecru.vercel.app/api/user";
 
   // Signup
   const signup = async (name, email, password) => {
